661bc2fe4af050eed93a4f8fca73612a7fbfc591
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-08-10 13:47:48 +0530

    Update deepgram.js

--- utils/deepgram.js
+++ utils/deepgram.js
@@ -1,17 +1,17 @@
 import { createClient } from '@deepgram/sdk';
 
 const deepgramApiKey = process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY;
-const deepgram = new createClient(deepgramApiKey);
+const deepgram = createClient(deepgramApiKey);
 
 export async function transcribeAudio(file) {
   try {
-    const response = await deepgram.transcription.preRecorded({
-      buffer: file,
-      mimetype: 'video/mp4'
-    }, {
-      punctuate: true,
-      utterances: true
-    });
+    const arrayBuffer = await file.arrayBuffer(); // Convert the file to ArrayBuffer
+    const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
+
+    const response = await deepgram.transcription.preRecorded(
+      { buffer: buffer, mimetype: 'video/mp4' },
+      { punctuate: true, utterances: true }
+    );
 
     const subtitles = response.results.utterances.map((utterance) => ({
       startTime: utterance.start,
